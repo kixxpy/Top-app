c84c266364001f9646caf35058a801314a11c002
Kirill Eliseev <ekn62@bk.ru>
2023-05-12 22:58:20 +0300

    first commit

---
+++ components/Tag/Tag.tsx
@@ -0,0 +1,24 @@
+import { TagProps } from './Tag.props';
+import styles from './Tag.module.css';
+import cn from 'classnames';
+
+
+export const Tag = ({size = 'm', children, color = 'ghost', href,  className, ...props}: TagProps): JSX.Element => {
+	return (
+		<div className={cn(styles.tag, className, {
+			[styles.s]: size == 's',
+			[styles.m]: size == 'm',
+			[styles.ghost]: color == 'ghost',
+			[styles.grey]: color == 'grey',
+			[styles.red]: color == 'red',
+			[styles.green]: color == 'green',
+			[styles.primary]: color == 'primary',
+		})}
+		{...props}
+		>
+			{
+				href ? <a href={href}>{children}</a> : <>{children}</>
+			}
+		</div>
+	);
+};
--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  //whatever config you have
+  //...
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+// eslint-disable-next-line no-undef
+module.exports = nextConfig;
